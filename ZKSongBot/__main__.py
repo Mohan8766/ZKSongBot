5c7db5a4a7304214e811a1a8a4312e4a30150af3
Mohan8766 <51364107+Mohan8766@users.noreply.github.com>
2021-08-08 19:57:45 +0530

    Update __main__.py

--- ZKSongBot/__main__.py
+++ ZKSongBot/__main__.py
@@ -2,11 +2,11 @@
 # @iamlooter1
 
 from pyrogram.types.bots_and_keyboards import reply_keyboard_markup
-from ZKSongBot.plugins import *
+from Song_Downloader_Bot.plugins import *
 from pyrogram import idle, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from ZKSongBot import Jebot as app
-from ZKSongBot import LOGGER
+from Song Downloader Bot import Jebot as app
+from Song Downloader Bot import LOGGER
 
 pm_start_text = """
 <b>Hi [{}](tg://user?id={}), I'm Song Downloader Bot.</b>
